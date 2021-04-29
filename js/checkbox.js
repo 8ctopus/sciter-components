c8d3040ab8667853d2c8ef17abb4bc33aa2a5f76
8ctopus <hello@octopuslabs.io>
2021-04-29 11:13:46 +0500

    improved: checkbox code

--- js/checkbox.js
+++ js/checkbox.js
@@ -8,7 +8,7 @@ export class Checkbox extends Element
         const id    = this.attributes["id"] || "?";
         const label = this.attributes["label"] || "?";
 
-        // option 1
+        // option 1 using one outermost element
         const component1 = (
             <div>
                 <input type="checkbox" id="{id}" name="{id}"></input>
@@ -16,11 +16,19 @@ export class Checkbox extends Element
             </div>
         );
 
-        // option 2
+        // option 2 using an array
         const component2 = [
             <input type="checkbox" id="{id}" name="{id}"></input>,
             <label for="{id}">{label}</label>
         ];
+
+        /*
+        doesn't work
+        const component3 = [
+            <input|checkbox #{id} ({id})></input>,
+            <label for="{id}">{label}</label>
+        ];
+        */
 
         this.content(component2);
     }
