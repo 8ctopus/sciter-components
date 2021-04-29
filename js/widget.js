1d76728a75f9b759276c99135a1a55fd7cd61f3d
8ctopus <hello@octopuslabs.io>
2021-04-29 09:05:39 +0500

    added: widget sample component

---
+++ js/widget.js
@@ -0,0 +1,25 @@
+export class Widget extends Element
+{
+    componentDidMount()
+    {
+        const message = this.attributes["message"] || "?";
+
+        const component = [
+            <h1>{message}</h1>,
+            <button .expand>+</button>,
+            <button .collapse>-</button>,
+        ];
+
+        this.content(component);
+    }
+
+    ["on click at button.expand"]()
+    {
+        this.state.expanded = true;
+    }
+
+    ["on click at button.collapse"]()
+    {
+        this.state.collapsed = true;
+    }
+}
