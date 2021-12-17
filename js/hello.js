6398c478ef8acbd99da5dc6c6beff6181fe3d0a3
8ctopus <hello@octopuslabs.io>
2021-12-17 10:55:02 +0500

    cleanup: hello component code comments

--- js/hello.js
+++ js/hello.js
@@ -2,13 +2,16 @@ export class Hello extends Element
 {
     /**
      * Called when element is attached to the DOM tree
+     * @return void
      */
     componentDidMount()
     {
+        // get user attribute
         const user = this.attributes["user"] || "?";
 
         const component = <h1>Hello {user}!</h1>;
 
+        // replace element content with component
         this.content(component);
     }
 }
